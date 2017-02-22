495c4e94238b9e5efb0f9fbe9af06f107caec90d
Cooper Mayne <coopermayne@gmail.com>
2017-02-22 15:59:54 -0800

    try memcache

--- Gemfile
+++ Gemfile
@@ -6,6 +6,10 @@ gem 'rails', '4.2.5'
 # Use postgresql as the database for Active Record
 gem 'pg', '~> 0.15'
 
+gem 'rack-cache'
+gem 'dalli'
+gem "memcachier"
+
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,22 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  client = Dalli::Client.new((ENV["MEMCACHIER_SERVERS"] || "").split(","),
+                             :username => ENV["MEMCACHIER_USERNAME"],
+                             :password => ENV["MEMCACHIER_PASSWORD"],
+                             :failover => true,
+                             :socket_timeout => 1.5,
+                             :socket_failure_delay => 0.2,
+                             :value_max_bytes => 10485760)
+  config.action_dispatch.rack_cache = {
+    :metastore    => client,
+    :entitystore  => client
+  }
+  config.static_cache_control = "public, max-age=2592000"
+
+  config.serve_static_assets = true
+
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
@@ -23,6 +39,8 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+
+
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
