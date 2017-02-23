b71d30d4dfd10dcb54b1c659a2434f4c6abb657d
Cooper Mayne <coopermayne@gmail.com>
2017-02-22 16:13:30 -0800

    basic cache working

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -1,80 +1,89 @@
 class MenuController < ApplicationController
-  def links
-  end
-
   def index
-    all_slides = Slide.get_all_slides
-      
-    news = NewsItem.news_box_items     
+    location_name = 'test'
+    in_cache = Rails.cache.read(location_name)
+
+    if in_cache
+      res = in_cache
+      message = "hit"
+    else
+      res = Project.first.title
+      message = "miss"
+
+      Rails.cache.write(location_name, res)
+    end
+
+    render :inline => "#{res}<br />cache: #{message}"
 
 
+    #all_slides = Slide.get_all_slides
+    #news = NewsItem.news_box_items     
+    #now_slides = all_slides["Now Institute"]
+    #home_slides = all_slides["Home Page"]
 
-    now_slides = all_slides["Now Institute"]
-    home_slides = all_slides["Home Page"]
+    #res = {}
+    #now_section = Section.find_by_title("Now Institute")
+    #res[:sections] = [
+      #{
+      #title: 'Now',
+      #slides: now_slides,
+      #state: 'root.section-state',
+      #url: "about",
+      #contact: now_section.contact,
+      #about: now_section.about,
+      #sorting: [ {
+        #title: 'Contact',
+        #items: ''
+      #}, {
+        #title: 'People',
+        #items: ['Leadership']
+      #}, {
+        #title: 'Media',
+        #items: ['Books', 'Bibliography', 'Awards']
+        ##items: ['Books', 'Bibliography', 'Videos', 'Awards', 'Exhibitions']
+      #} ]
+    #},
 
-    res = {}
-    now_section = Section.find_by_title("Now Institute")
-    res[:sections] = [
-      {
-      title: 'Now',
-      slides: now_slides,
-      state: 'root.section-state',
-      url: "about",
-      contact: now_section.contact,
-      about: now_section.about,
-      sorting: [ {
-        title: 'Contact',
-        items: ''
-      }, {
-        title: 'People',
-        items: ['Leadership']
-      }, {
-        title: 'Media',
-        items: ['Books', 'Bibliography', 'Awards']
-        #items: ['Books', 'Bibliography', 'Videos', 'Awards', 'Exhibitions']
-      } ]
-    },
+    #{
+      #title: 'Projects',
+      #url: "projects",
+      #state: 'root.section-state.sorting-state',
+      #sorting: [ {
+        #title: 'A-Z',
+        #items: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+      #}, {
+        #title: 'Year',
+        #items: [ '1970-1979', '1980-1989', '1990-1999', '2000-2009', '2010-2019' ].reverse
+      #}, {
+        #title: 'Type',
+        #items: Section.find_by_title("Projects").get_types
+      #} ]
+    #}, 
 
-    {
-      title: 'Projects',
-      url: "projects",
-      state: 'root.section-state.sorting-state',
-      sorting: [ {
-        title: 'A-Z',
-        items: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-      }, {
-        title: 'Year',
-        items: [ '1970-1979', '1980-1989', '1990-1999', '2000-2009', '2010-2019' ].reverse
-      }, {
-        title: 'Type',
-        items: Section.find_by_title("Projects").get_types
-      } ]
-    }, 
+    #{
+      #title: 'Research',
+      #url: "research",
+      #state: 'root.section-state.sorting-state',
+      #sorting: [ {
+        #title: 'A-Z',
+        #items: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+      #}, {
+        #title: 'Year',
+        #items: [ '1970-1979', '1980-1989', '1990-1999', '2000-2009', '2010-2019' ].reverse
+      #}, {
+        #title: 'Type',
+        #items: Section.find_by_title("Research").get_types
+      #} ]
+    #}, 
 
-    {
-      title: 'Research',
-      url: "research",
-      state: 'root.section-state.sorting-state',
-      sorting: [ {
-        title: 'A-Z',
-        items: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-      }, {
-        title: 'Year',
-        items: [ '1970-1979', '1980-1989', '1990-1999', '2000-2009', '2010-2019' ].reverse
-      }, {
-        title: 'Type',
-        items: Section.find_by_title("Research").get_types
-      } ]
-    }, 
-
-    {
-      title: 'News',
-      url: 'news',
-      state: 'root.section-state',
-      items: news
-    }
-    ]
-    render json: res
+    #{
+      #title: 'News',
+      #url: 'news',
+      #state: 'root.section-state',
+      #items: news
+    #}
+    #]
+    #render json: res
   end
 
 	def videos
