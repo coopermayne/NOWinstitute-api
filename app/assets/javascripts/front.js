d72c4431285d2f5a2a4da7f8654304c97eb1e7d5
Cooper Mayne <coopermayne@gmail.com>
2017-03-02 16:18:57 -0800

    grrrrrr

--- app/assets/javascripts/front.js
+++ app/assets/javascripts/front.js
@@ -1,7 +1,7 @@
 //= require jquery
 //= require jquery.unveil
+//= require jquery.photoswipe-global
 //= require turbolinks
-//= require photoswipe
 
 $(document).on('turbolinks:load', function(){
 
@@ -68,44 +68,6 @@ $(document).on('turbolinks:load', function(){
     }
   })
 
-  $('.picture').each(function(){
-    var $pic     = $(this),
-      getItems = function() {
-        var items = [];
-        $pic.find('a').each(function() {
-          var $href   = $(this).attr('href'),
-            $size   = $(this).data('size').split('x'),
-              $width  = $size[0],
-                $height = $size[1];
-
-                var item = {
-                  src : $href,
-                  w   : $width,
-                  h   : $height
-                }
-
-                items.push(item);
-        });
-        return items;
-      }
-
-      var items = getItems();
-      console.log(items)
-  })
-
-  var $pswp = $('.pswp')[0];
-  $pic.on('click', 'figure', function(event) {
-    event.preventDefault();
-
-    var $index = $(this).index();
-    var options = {
-      index: $index,
-      bgOpacity: 0.7,
-      showHideOpacity: true
-    }
-
-    var lightBox = new PhotoSwipe($pswp, PhotoSwipeUI_Default, items, options);
-    lightBox.init();
-  });
+  $('#gallery').photoSwipe(".slide", {bgOpacity: 0.8, shareEl: false}, {});
 
 })
