0fbc9beacb1e29d0f843c08d8de7c113ab290e84
Cooper Mayne <coopermayne@gmail.com>
2017-03-04 20:27:51 -0800

    mobile menu cover

--- app/assets/javascripts/front.js
+++ app/assets/javascripts/front.js
@@ -73,8 +73,8 @@ $(document).on('turbolinks:load', function(){
     e.stopPropagation();
     console.log("clicked ham");
 
-    $('nav').toggleClass('hide')
-    $('nav').toggleClass('cover')
+    $('.menu-directive').toggleClass('contracted-menu')
+    $('.menu-directive').toggleClass('cover')
     $('#hamburger').toggleClass('hide')
     $('.magic-x').toggleClass('hide')
   })
