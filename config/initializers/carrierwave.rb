d71ebb3aca6c0c4c1fc916e052fc783dd5f73a24
Cooper Mayne <coopermayne@gmail.com>
2017-03-06 13:07:59 -0800

    unnec

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,5 @@
 module CarrierWave
   module MiniMagick
-    #def quality(percentage)
-      #manipulate! do |img|
-        #img.quality(percentage.to_s)
-        #img = yield(img) if block_given?
-        #img
-      #end
-    #end
 
     def optimize
       manipulate! do |img|
