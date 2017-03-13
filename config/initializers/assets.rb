582b02076292e659802487a44106c5a6546a9e0f
Cooper Mayne <coopermayne@gmail.com>
2017-03-13 13:10:16 -0700

    move fonts to their proper place

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,8 +5,8 @@ Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path
  #Rails.application.config.assets.paths << "/assets/fonts"
-#Rails.application.config.assets.paths << Rails.root.join("app", "assets", "fonts")
+Rails.application.config.assets.paths << Rails.root.join("app", "assets", "fonts")
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w(front.css front.js end.js)
+Rails.application.config.assets.precompile += %w(front.css front.js end.js .svg .eot .woff .ttf)
